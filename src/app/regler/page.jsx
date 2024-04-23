10df9f59904f7daa0022ab8fba823862e9e89796
Melbye <simo80k3@stud.kea.dk>
2024-04-24 00:05:53 +0200

    font size

--- src/app/regler/page.jsx
+++ src/app/regler/page.jsx
@@ -29,14 +29,14 @@ export default function Regler() {
         <div className={styles.gridContainer}>
             <div className={styles.textcol}>
                 <h1 className='text-xl'>Regler</h1>
-                <p>Denne side indeholder en tabel over Axe's HTML 4.9 tilgængelighedsregler, designet til at guide webudviklere og designere i at opnå bedre digital tilgængelighed.</p>
+                <p className={styles.paragraph}>Denne side indeholder en tabel over Axe's HTML 4.9 tilgængelighedsregler, designet til at guide webudviklere og designere i at opnå bedre digital tilgængelighed.</p>
             </div>
             <div className={styles.griditem}>
                 <table className={styles.table}>
                     <thead>
                         <tr>
                             <th className={styles.headerCell}>Navn på regel</th>
-                            <th className={styles.headerCell}>Beskrivelse</th>
+                            <th className={`${styles.headerCell} ${styles.descColumn}`}>Beskrivelse</th>
                             <th className={styles.headerCell}>Vigtighed</th>
                         </tr>
                     </thead>
@@ -44,7 +44,7 @@ export default function Regler() {
 {simonData.map((item, index) => (
   <tr key={item.slug} className={(index % 2 === 0) ? styles.tableRowEven : ''}>
     <td className={styles.dataCell}>{item.name}</td>
-    <td className={styles.dataCell}>{item.shortDesc}</td>
+    <td className={`${styles.dataCell} ${styles.descColumn}`}>{item.shortDesc}</td>
     <td className={`${styles.dataCell} ${getImpactClassName(item.impact)}`}>{item.impact}</td>
     <td className={`${styles.dataCell} ${styles.lastColumn}`}>
       <a className={styles.link} href={`/rules/${item.slug}`}>Read more</a>
