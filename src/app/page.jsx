a9590d94f31d47df2373b3d798219cc667462c62
NL <nila0001@stud.kea.dk>
2024-04-23 17:48:51 +0200

    ret tekst index og hover link nav

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,11 +3,11 @@ export default function Home() {
   return (
     <section className="grid place-content-center text-center gap-5 m-10">
       <h1 className="text-xl">Web tilgængeligheds validater</h1>
-      <p className="max-w-[65ch] text-sm">Indsat hjemmesidens adresse og få en rapport på hvordan tilgængeligheden på dit website ser ud</p>
+      <p className="max-w-[65ch] text-sm">Indsat hjemmesidens adresse og få en rapport på hvordan tilgængeligheden på dit website ser ud. Husk at skriv "https://" efterfult af hjemmeside adressen</p>
       <form action="/resultat">
         <label htmlFor="form_url" className="flex flex-wrap px-10 justify-center gap-2 sm:gap-0">
           <p className="sr-only">Skriv din url her</p>
-          <input className="rounded-l border-2 border-state-3 p-2 flex-0 w-full sm:flex-1" type="url" name="url" id="form_url" required placeholder="Indtast URL.." />
+          <input className="rounded-l border-2 border-state-3 p-2 flex-0 w-full sm:flex-1" type="url" name="url" id="form_url" required placeholder="https://URL..." />
           <Butten variant="input">Analyser</Butten>
         </label>
       </form>
--- src/components/Linknavigation.jsx
+++ src/components/Linknavigation.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function LinkNavigation({ children, path }) {
   return (
     <li>
-      <Link className="hover:text-underline" prefetch={false} href={path}>
+      <Link className="hover:underline" prefetch={false} href={path}>
         {children}
       </Link>
     </li>
