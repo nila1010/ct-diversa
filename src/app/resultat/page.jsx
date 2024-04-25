9497884f5905eba5c1087ef0b65ad0c21b8c209f
AndreasEkvall <anek0001@stud.kea.dk>
2024-04-25 12:14:16 +0200

    betinget visning

--- src/app/resultat/page.jsx
+++ src/app/resultat/page.jsx
@@ -1,49 +1,67 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import ListItem from '@/components/ListItem';
 
 export const revalidate = 1800;
 
 export default function Resultat() {
 
+
+    const data = require('./dummyData.json');
+    const violations = data.violations;
+    const critical = violations.filter(one => one.impact === 'critical')
+    const serious = violations.filter(one => one.impact === 'serious')
+    const major = critical.concat(serious)
+    const moderate = violations.filter(one => one.impact === 'moderate')
+    const minor = violations.filter(one => one.impact === 'minor')
+
     return (
     <>
-        <section className="mx-auto px-0 my-10">
+        <section className="px-0 my-10">
             <article className="grid grid-cols-1 gap-x-12 gap-y-8 md:grid-cols-2">
                 <figure className="flex flex-col w-auto justify-end place-content-right gap-y-2 md:order-2">
                     <Image className="w-auto object-contain" src="/dummyImg.svg" alt="Logo" width="300" height="300" />
                     <h3 className='w-auto text-lg text-center'>Overall score</h3>
                 </figure>
                 <figure className="flex flex-col place-content-center gap-y-2 md:order-1">
-                    <h2 className="text-xl text-state-2">Din url er her: Data</h2>
-                    <p className='text-pretty'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus provident doloribus ipsum aliquid autem quod blanditiis soluta facere quisquam distinctio, sequi voluptatum consectetur iure cum odit accusamus eligendi commodi. Ab.</p>
-                    <Image className="w-auto object-contain" src="/dummyImg.svg" alt="Logo" width={640} height={480}/>
+                    <h2 className="text-xl text-state-2">{data.url}</h2>
+                    <p className='text-pretty'></p>
+                    <Image className="w-auto object-contain" src="/dummyImg.svg" alt="Logo" width={data.screenshot.width} height={data.screenshot.height}/>
                 </figure>
             </article>
         </section>
 
         <section className='flex flex-col gap-y-6 my-14 px-0'>
             <h2 className='text-2xl'>Fuld raport </h2>
-            <details className='bg-state-3-light'>
-                <summary className='flex gap-3 justify-between text-xl p-6 border-solid border-4 rounded border-state-3 after:content-[">"]'><div><span>XXX</span> kritiske fejl</div></summary>
-                <ul className='flex flex-col gap-y-8 p-6 my-3 mx-5'>
-                    <ListItem></ListItem>
-                    <ListItem></ListItem>
-                </ul>
-            </details>
-            <details className='bg-state-2-light'>
-                <summary className='flex gap-3 justify-between text-xl p-6 border-solid border-4 rounded border-state-2 after:content-[">"]'><div><span>XXX</span> moderate fejl</div></summary>
-                <ul className='flex flex-col gap-y-6 p-6 my-3 mx-5'>
-                    <ListItem></ListItem>
-                    <ListItem></ListItem>
-                </ul>
-            </details>
+            {major.length != 0 && (
+                <details className='bg-state-3-light'>
+                    <summary className='flex gap-3 justify-between text-xl p-6 border-solid border-4 rounded border-state-3 after:content-[">"]'><div><span>{major.length}</span> kritiske fejl</div></summary>
+                    <ul className='flex flex-col gap-y-8 p-6 my-3 mx-5'>
+                        {major.map((d) => (
+                            <ListItem key={d.id} error={d.id} description={d.description} />
+                        ))}
+                    </ul>
+                </details>
+            )}
+            {moderate.length != 0 && (
+                <details className='bg-state-2-light'>
+                    <summary className='flex gap-3 justify-between text-xl p-6 border-solid border-4 rounded border-state-2 after:content-[">"]'><div><span>{moderate.length}</span> moderate fejl</div></summary>
+                    <ul className='flex flex-col gap-y-6 p-6 my-3 mx-5'>
+                    {moderate.map((d) => (
+                            <ListItem key={d.id} error={d.id} description={d.description} />
+                        ))}
+                    </ul>
+                </details>
+            )}
+            {minor.length != 0 && (
             <details className='bg-state-1-light'>
-                <summary className='flex gap-3 justify-between text-xl p-6 border-solid border-4 rounded border-state-1 after:content-[">"]'><div><span>XXX</span> mindre fejl</div></summary>
+                <summary className='flex gap-3 justify-between text-xl p-6 border-solid border-4 rounded border-state-1 after:content-[">"]'><div><span>{minor.length}</span> mindre fejl</div></summary>
                 <ul className='flex flex-col gap-y-6 p-6 my-3 mx-5'>
-                    <ListItem></ListItem>
-                    <ListItem></ListItem>
+                {minor.map((d) => (
+                        <ListItem key={d.id} error={d.id} description={d.description} />
+                    ))}
                 </ul>
             </details>
+            )}
         </section>
     </>
 )
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,10 @@
 import Butten from "./Butten";
-export default function ListItem() {
+export default function ListItem({error, description}) {
     return (
     <li className='flex flex-wrap gap-3 flex-row w-full justify-between items-center'>
         <div className='max-w-prose'>
-            <h3 className='text-lg'>Dit fejl</h3>
-            <span className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ratione soluta repudiandae enim id, distinctio officiis repellendus? Similique repellendus labore accusantium voluptatem itaque quos rerum maxime! Reprehenderit quasi expedita quae.</span>
+            <h3 className=' text-lg'>{error}</h3>
+            <span className='text-sm'>{description}</span>
         </div>
         <Butten variant="input">Læs mere om fejlen</Butten>
     </li>
