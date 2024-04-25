aec5a9afd07d42703a4ba5b6eb29a341dd4902a8
NL <nila0001@stud.kea.dk>
2024-04-25 09:03:06 +0200

    index_v2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <section className="grid place-content-center text-center gap-5 m-10">
       <h1 className="text-xl">Web tilgængeligheds validater</h1>
-      <p className="max-w-[65ch] text-sm">Indsat hjemmesidens adresse og få en rapport på hvordan tilgængeligheden på dit website ser ud. Husk at skriv "https://" efterfult af hjemmeside adressen</p>
+      <p className="max-w-[65ch] text-sm">Indsat hjemmesidens adresse og få en rapport på hvordan tilgængeligheden på dit website ser ud. Husk at skriv https:// efterfult af hjemmeside adressen</p>
       <form action="/resultat">
         <label htmlFor="form_url" className="flex flex-wrap px-10 justify-center gap-2 sm:gap-0">
           <p className="sr-only">Skriv din url her</p>
