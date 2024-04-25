0492a3fbb13a491ddea7957995c426b073fde914
Melbye <simo80k3@stud.kea.dk>
2024-04-25 12:25:54 +0200

    button

--- src/components/Butten.jsx
+++ src/components/Butten.jsx
@@ -1,5 +1,5 @@
 const variants = {
-  input: "bg-state-3 py-2 px-5 rounded-r text-lg hover:text-backgound-color-secondary transition",
+  input: "bg-state-3 py-2 px-5 rounded-r text-sm hover:text-backgound-color-secondary transition",
   error: "bg-error-color py-2 px-5 rounded text-lg hover:text-state-3 transition",
   regl: "bg-state-3 py-2 px-5 rounded-r text-sm hover:text-backgound-color-secondary transition w-full",
 };
