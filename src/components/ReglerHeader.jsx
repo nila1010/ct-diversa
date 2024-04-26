c73d3530ff5bea2a1a7540c0a02f41a836ba3c46
NL <nila0001@stud.kea.dk>
2024-04-26 08:34:47 +0200

    axe fix reglheader

--- src/components/ReglerHeader.jsx
+++ src/components/ReglerHeader.jsx
@@ -4,7 +4,7 @@ import styles from "./Regler.module.css";
 const ReglerHeader = () => (
   <div className={styles.textcol}>
     <h1 className="text-xl">Regler</h1>
-    <p className={`${styles.paragraph} text-sm`}>Denne side indeholder en tabel over Axe's HTML 4.9 tilgængelighedsregler, designet til at guide webudviklere og designere i at opnå bedre digital tilgængelighed.</p>
+    <p className={`${styles.paragraph} text-sm`}>Denne side indeholder en tabel over Axes HTML 4.9 tilgængelighedsregler, designet til at guide webudviklere og designere i at opnå bedre digital tilgængelighed.</p>
   </div>
 );
 
