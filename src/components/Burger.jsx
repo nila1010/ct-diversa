ef742a5bd9a572694c8fdee07a56c7b8dc074833
NL <nila0001@stud.kea.dk>
2024-04-26 12:20:13 +0200

    v1

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -7,13 +7,15 @@ import "animate.css";
 function Burger() {
   const [isOpen, setOpen] = useState(false);
 
+  function closeMenu() {
+    setOpen(false);
+  }
   return (
     <>
-      {" "}
       {isOpen && (
-        <ul className="absolute top-[70%] right-0 bg-backgound-color-secondary z-10 grid gap-2 p-5 animate__bounceInDown animate__animated">
+        <ul onClick={closeMenu} className="absolute top-[65%] right-0 bg-backgound-color-secondary z-10 grid gap-2 pt-10 px-8 pb-4 text-md rounded-b animate__bounceInDown animate__animated">
           <Linknavigation path="/#">Check site</Linknavigation>
-          <Linknavigation path="/#">Regler</Linknavigation>
+          <Linknavigation path="/regler">Regler</Linknavigation>
         </ul>
       )}
       <Hamburger toggled={isOpen} toggle={setOpen} />
