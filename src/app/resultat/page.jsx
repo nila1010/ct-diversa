e47a72ab13e4735a2922c44f54b8fe3c75a1668b
AndreasEkvall <anek0001@stud.kea.dk>
2024-04-29 19:42:51 +0200

    summary arrow

--- src/app/resultat/page.jsx
+++ src/app/resultat/page.jsx
@@ -16,7 +16,6 @@ export default async function Resultat({ searchParams }) {
   const major = critical.concat(serious);
   const moderate = violations.filter((one) => one.impact === "moderate");
   const minor = violations.filter((one) => one.impact === "minor");
-
   const total = score(data);
   const webScore = total[0];
 
@@ -24,16 +23,16 @@ export default async function Resultat({ searchParams }) {
     <>
       <div className="m-10">
         <section className="mb-10">
-          <article className="grid grid-cols-1 gap-x-12 gap-y-8 md:grid-cols-2">
-            <figure className="flex flex-col w-auto place-content-center gap-y-2 md:order-2">
+          <article className="grid grid-cols-1 gap-x-12 gap-y-8 sm:grid-cols-2">
+            <div className="flex flex-col w-auto place-content-center gap-y-2 sm:order-2">
               <AnimatedCircle data={webScore} />
-              <h2 className="w-auto text-md text-center">Overall score</h2>
-            </figure>
-            <figure className="flex flex-col gap-y-2 md:order-1">
-              <h1 className="text-xl text-balance">{data.url}</h1>
-              <p className="text-pretty">Nedenfor kan du se en raport over de tilgængelighedsprobler vi har fundet. De er opdelt i 3 kategorier hvor kritiske fejl kan forårsage at brugeren ikke kan bruge hjemmesiden. moderete fejl gør det ikke er optimalt for brugeren og de mindre fejl er et eller andet</p>
+              <h3 className="w-auto text-md text-center">Overall score</h3>
+            </div>
+            <div className="flex flex-col text-pretty gap-y-4 px-0 sm:order-1">
+              <h2 className="text-xl">{data.url}</h2>
+              <p className="text-sm">Nedenfor kan du se en raport over de tilgængelighedsprobler vi har fundet. De er opdelt i 3 kategorier hvor kritiske fejl kan forårsage at brugeren ikke kan bruge hjemmesiden. Moderate fejl gør det ikke er optimalt for brugeren og de mindre fejl er et eller andet</p>
               <Image className="w-auto object-contain" src={data.screenshot.url} alt="Logo" width={data.screenshot.width} height={data.screenshot.height} />
-            </figure>
+            </div>
           </article>
         </section>
 
@@ -41,7 +40,7 @@ export default async function Resultat({ searchParams }) {
           <h3 className="text-xl">Fuld raport </h3>
           {major.length != 0 && (
             <details className="bg-state-2-light  border-state-3 border-4 rounded border-solid open:transition-all group">
-              <summary className='flex gap-3 bg-state-3 justify-between text-lg p-6 hover:cursor-pointer after:content-[">"] group-open:transition group-open:after:rotate-90'>
+              <summary className='flex gap-3 bg-state-3 justify-between align-middle text-lg p-6 hover:cursor-pointer after:content-[">"] group-open:after:transition-all group-open:after:rotate-90'>
                 <div>
                   <span>{major.length}</span> kritiske fejl
                 </div>
@@ -54,9 +53,9 @@ export default async function Resultat({ searchParams }) {
             </details>
           )}
           {moderate.length != 0 && (
-            <details className="bg-state-2-light rounded border-state-2 border-4 border-solid open:transition-all group">
-              <summary className='flex w-full h-fit bg-state-2 justify-between text-lg p-6 hover:cursor-pointer after:content-[">"] group-open:transition-all group-open:after:rotate-90'>
-                <div>
+            <details className="bg-state-2-light rounded border-state-2 align-center border-4 border-solid open:transition-all transform group">
+              <summary className='flex place-items-center bg-state-2 justify-between text-lg px-3 hover:cursor-pointer after:text-2xl after:content-[">"] group-open:after:transition-all group-open:after:rotate-90 group-open:after:text-2xl'>
+                <div className="">
                   <span>{moderate.length}</span> moderate fejl
                 </div>
               </summary>
@@ -69,7 +68,7 @@ export default async function Resultat({ searchParams }) {
           )}
           {minor.length != 0 && (
             <details className="bg-state-2-light border-state-1 rounded border-solid border-4 open:transition-all group">
-              <summary className='flex gap-3 bg-state-1 justify-between text-lg p-6 hover:cursor-pointer after:content-[">"] group-open:transition group-open:after:rotate-90'>
+              <summary className='flex gap-3 bg-state-1 justify-between text-lg p-6 hover:cursor-pointer after:content-[">"] group-open:after:transition-all group-open:after:rotate-90'>
                 <div>
                   <span>{minor.length}</span> mindre fejl
                 </div>
