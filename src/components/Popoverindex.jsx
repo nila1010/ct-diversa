e3ef89a49827ddec8b8158bedb65542c82c1a49a
Melbye <simo80k3@stud.kea.dk>
2024-04-29 18:00:57 +0200

    popup

--- src/components/Popoverindex.jsx
+++ src/components/Popoverindex.jsx
@@ -6,7 +6,18 @@ function Popoverindex() {
       </button>
       <div popover="manual" id="pop" className="absolute bottom-[-340px] max-w-[400px] bg-state-1-light outline-state-1 outline outline-2 rounded px-5 pb-3">
         <div className="flex place-content-center gap-5 ">
-          <p className="text-left">Vi tester sitet for om det overholder standarder og giver dig en raport tilbage med mere overblik</p>
+        <p className="text-left">
+  Vi sikrer vores indholds tilgængelighed ved at teste efter WCAG 2.1 guidelines på Level A, AA, AAA og bedste praksis. For mere detaljeret information, besøg
+  <span class="mr-2"></span>
+  <a href="https://dequeuniversity.com/rules/axe/html/4.9" target="_blank" class="text-state-3 hover:text-state-3-light underline">
+    Deque University
+  </a>.
+</p>
+
+
+
+
+
           <button className="mb-5 mt-2" popovertarget="pop" popovertargetaction="hide">
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#FF7733" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
               <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
