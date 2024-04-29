b35ab1ebf605106c1cea667b569f956ca564e589
AndreasEkvall <anek0001@stud.kea.dk>
2024-04-29 20:14:33 +0200

    summary / listitem

--- src/app/resultat/page.jsx
+++ src/app/resultat/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import ListItem from "@/components/ListItem";
+import ViolationList from "@/components/ViolationList";
 import AnimatedCircle from "@/components/AnimatedCircle";
+import dummyData from '@/data/dummy'
 import { score } from "@/lib/CalcRating";
 
 export const revalidate = 1800;
@@ -21,7 +22,7 @@ export default async function Resultat({ searchParams }) {
 
   return (
     <>
-      <div className="m-10">
+      <div className="m-2 sm:m-10">
         <section className="mb-10">
           <article className="grid grid-cols-1 gap-x-12 gap-y-8 sm:grid-cols-2">
             <div className="flex flex-col w-auto place-content-center gap-y-2 sm:order-2">
@@ -29,7 +30,7 @@ export default async function Resultat({ searchParams }) {
               <h3 className="w-auto text-md text-center">Overall score</h3>
             </div>
             <div className="flex flex-col text-pretty gap-y-4 px-0 sm:order-1">
-              <h2 className="text-xl">{data.url}</h2>
+              <h2 className="text-xl w-full">{data.url}</h2>
               <p className="text-sm">Nedenfor kan du se en raport over de tilgængelighedsprobler vi har fundet. De er opdelt i 3 kategorier hvor kritiske fejl kan forårsage at brugeren ikke kan bruge hjemmesiden. Moderate fejl gør det ikke er optimalt for brugeren og de mindre fejl er et eller andet</p>
               <Image className="w-auto object-contain" src={data.screenshot.url} alt="Logo" width={data.screenshot.width} height={data.screenshot.height} />
             </div>
@@ -39,43 +40,43 @@ export default async function Resultat({ searchParams }) {
         <section className="flex flex-col gap-y-6 my-14 px-0">
           <h3 className="text-xl">Fuld raport </h3>
           {major.length != 0 && (
-            <details className="bg-state-2-light  border-state-3 border-4 rounded border-solid open:transition-all group">
-              <summary className='flex gap-3 bg-state-3 justify-between align-middle text-lg p-6 hover:cursor-pointer after:content-[">"] group-open:after:transition-all group-open:after:rotate-90'>
+            <details className="bg-state-2-light  border-state-3 border-4 rounded border-solid open:transition-all transform group">
+              <summary className='flex gap-3 bg-state-3 justify-between place-items-center text-lg px-2 hover:cursor-pointer sm:px-6 md:px-10 after:text-2xl after:content-[">"] group-open:after:transition-all group-open:after:rotate-90 group-open:after:text-2xl'>
                 <div>
                   <span>{major.length}</span> kritiske fejl
                 </div>
               </summary>
               <ul className="flex flex-col gap-y-8 p-6 my-3 mx-5 justify-between">
                 {major.map((d) => (
-                  <ListItem key={d.id} error={d.id} description={d.description} />
+                  <ViolationList key={d.id} error={d.id} description={d.description} />
                 ))}
               </ul>
             </details>
           )}
           {moderate.length != 0 && (
             <details className="bg-state-2-light rounded border-state-2 align-center border-4 border-solid open:transition-all transform group">
-              <summary className='flex place-items-center bg-state-2 justify-between text-lg px-3 hover:cursor-pointer after:text-2xl after:content-[">"] group-open:after:transition-all group-open:after:rotate-90 group-open:after:text-2xl'>
-                <div className="">
+              <summary className='flex place-items-center bg-state-2 justify-between text-lg px-2 hover:cursor-pointer sm:px-6 md:px-10 after:text-2xl after:content-[">"] group-open:after:transition-all group-open:after:rotate-90 group-open:after:text-2xl'>
+                <div>
                   <span>{moderate.length}</span> moderate fejl
                 </div>
               </summary>
-              <ul className="flex flex-col gap-y-6 p-6 my-3 mx-5">
+              <ul className="flex flex-col gap-y-6 p-6 my-0 mx-0 sm:mx-5 sm:my-3">
                 {moderate.map((d) => (
-                  <ListItem key={d.id} error={d.id} description={d.description} />
+                  <ViolationList key={d.id} error={d.id} description={d.description} />
                 ))}
               </ul>
             </details>
           )}
           {minor.length != 0 && (
             <details className="bg-state-2-light border-state-1 rounded border-solid border-4 open:transition-all group">
-              <summary className='flex gap-3 bg-state-1 justify-between text-lg p-6 hover:cursor-pointer after:content-[">"] group-open:after:transition-all group-open:after:rotate-90'>
+              <summary className='flex gap-3 bg-state-1 justify-between text-lg px-2 sm:px-4 md:px-10 hover:cursor-pointer after:content-[">"] group-open:after:transition-all group-open:after:rotate-90'>
                 <div>
                   <span>{minor.length}</span> mindre fejl
                 </div>
               </summary>
               <ul className="flex flex-col gap-y-6 p-6 my-3 mx-5">
                 {minor.map((d) => (
-                  <ListItem key={d.id} error={d.id} description={d.description} />
+                  <ViolationList key={d.id} error={d.id} description={d.description} />
                 ))}
               </ul>
             </details>
--- src/components/ViolationList.jsx
+++ src/components/ViolationList.jsx
@@ -1,10 +1,10 @@
 import Butten from "./Butten";
-export default function ListItem({error, description}) {
+export default function ViolationList({error, description}) {
     return (
     <li className='flex flex-wrap gap-3 flex-row w-full justify-between items-center'>
-        <div className='w-3/5'>
+        <div className='w-full md:w-2/6'>
             <h3 className='text-lg first-letter:uppercase'>{error}</h3>
-            <span className='text-sm min-w-min'>{description}</span>
+            <span className='text-sm'>{description}</span>
         </div>
         <Butten path="regler" variant="primary">Læs mere om fejlen</Butten>
     </li>
