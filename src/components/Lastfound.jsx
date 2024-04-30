1e9c683e85608fce2e4c8821a9a241728eeda495
NL <nila0001@stud.kea.dk>
2024-04-30 13:58:10 +0200

    v1

--- src/components/Lastfound.jsx
+++ src/components/Lastfound.jsx
@@ -6,9 +6,9 @@ export default function Lastfound({ data }) {
       <details>
         <summary className="ml-[-2px] cursor-pointer py-2 px-5 text-sm relative before:rounded before:absolute before:inset-0 before:content-[''] before:border-2 before:border-state-2 before:-z-50 after:absolute after:inset-0 after:content-[''] after:-z-50 after:bg-state-2 after:w-full after:h-full hover:after:scale-y-[0.8] hover:after:scale-x-[0.95] after:transition after:duration-500 after:ease after:rounded">Dine seneste 5 søgninger</summary>
         <ul className="bg-state-1-light outline rounded outline-2 outline-state-2">
-          {data.map((o) => {
+          {data.map((o, i) => {
             return (
-              <li className="relative z-0 flex justify-between items-center p-2" key={o.searchUrl}>
+              <li className="relative gap-5 z-0 flex justify-between items-center p-2" key={i}>
                 <div className="text-left max-w-[18ch]">
                   <p className="truncate">Url: {o.searchUrl}</p>
                   <p>Dato: {o.date}</p>
