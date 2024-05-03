7cf2a00d7ace1d4c98f6d6fbf393acc8a44f5f11
Melbye <simo80k3@stud.kea.dk>
2024-05-03 09:48:49 +0200

    historik

--- src/components/Lastfound.jsx
+++ src/components/Lastfound.jsx
@@ -5,7 +5,7 @@ export default function Lastfound({ data }) {
   return (
     <section>
       <details>
-        <summary className="ml-[-2px] cursor-pointer py-2 px-5 text-sm relative before:rounded before:absolute before:inset-0 before:content-[''] before:border-2 before:border-state-2 before:-z-50 after:absolute after:inset-0 after:content-[''] after:-z-50 after:bg-state-2 after:w-full after:h-full hover:after:scale-y-[0.8] hover:after:scale-x-[0.95] after:transition after:duration-500 after:ease after:rounded">Dine seneste 5 søgninger</summary>
+        <summary className="ml-[-2px] cursor-pointer py-2 px-5 text-sm relative before:rounded before:absolute before:inset-0 before:content-[''] before:border-2 before:border-state-2 before:-z-50 after:absolute after:inset-0 after:content-[''] after:-z-50 after:bg-state-2 after:w-full after:h-full hover:after:scale-y-[0.8] hover:after:scale-x-[0.95] after:transition after:duration-500 after:ease after:rounded">Din Historik</summary>
         <ul className="bg-state-1-light outline rounded outline-2 outline-state-2">
           {data.map((o, i) => {
             return (
